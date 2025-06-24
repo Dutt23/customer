ca1639f27213b4efda038655b36a13e7a78d2801
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2025-06-25 05:14:30 +0530

    Add retry annotation

--- src/main/java/com/sd/customer/CustomerApplication.java
+++ src/main/java/com/sd/customer/CustomerApplication.java
@@ -2,10 +2,12 @@ package com.sd.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableRetry
 public class CustomerApplication {
 
     public static void main(String[] args) {
