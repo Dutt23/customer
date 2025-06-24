159d5a18776b73d30a9e3399938136fd59c7cbe3
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2025-06-25 02:44:49 +0530

    Add logging and health check

---
+++ src/main/java/com/sd/customer/middlewares/SimpleTimingMiddleware.java
@@ -0,0 +1,24 @@
+package com.sd.customer.middlewares;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class SimpleTimingMiddleware implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(SimpleTimingMiddleware.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        logger.info("Started request [{} {}]", req.getMethod(), req.getRequestURI());
+        long start = System.currentTimeMillis();
+        chain.doFilter(request, response);
+        long duration = System.currentTimeMillis() - start;
+        logger.info("Request [{} {}] completed in {} ms", req.getMethod(), req.getRequestURI(), duration);
+    }
+}
