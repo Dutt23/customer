15fc43ad7dfd12dab17dfc27479ccafe39b74b30
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2025-06-25 05:16:16 +0530

    fix error handling

--- src/main/java/com/sd/customer/controllers/CustomerExceptionController.java
+++ src/main/java/com/sd/customer/controllers/CustomerExceptionController.java
@@ -14,27 +14,13 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice(assignableTypes = {CustomerController.class})
 public class CustomerExceptionController {
 
     private static final Logger LOGGER = LogManager.getLogger(CustomerExceptionController.class);
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex) {
-        LOGGER.error("[CustomerExceptionController] Exception occurred while processing ", ex);
-        List<String> errors = ex.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(error -> error.getField() + ": " + error.getDefaultMessage())
-                .collect(Collectors.toList());
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleAllExceptions(Exception ex) {
@@ -43,7 +29,7 @@ public class CustomerExceptionController {
         errorBody.put("timestamp", LocalDateTime.now());
         errorBody.put("status", 400);
         errorBody.put("error", "Bad Request");
-        errorBody.put("message", ex.getMessage());
+        errorBody.put("message", Arrays.asList(errors));
         errorBody.put("type", ex.getClass());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(errorBody);
